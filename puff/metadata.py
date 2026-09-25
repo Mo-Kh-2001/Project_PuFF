cf2affbc07040a2366593e4dcfd39e6c8885ad45
Mo-Kh-2001 <mohammad.khavri.2001@gmail.com>
2026-09-25 14:01:34 +0100

    Add format, width, height to get_meta_data

--- puff/metadata.py
+++ puff/metadata.py
@@ -1,10 +1,14 @@
 from pathlib import Path
+from PIL import Image
 
 def get_meta_data(path):
     data = path.stat()
     size = data.st_size
     mtime =data.st_mtime
-    result = {"size":size,"mtime":mtime}
+    with Image.open(path) as img:
+        width, height = img.size
+        fmt = img.format
+    result = {"size":size,"mtime":mtime,"width":width,"height":height,"format":fmt}
     return result
 if __name__ == "__main__":
     print(get_meta_data(Path("data/2222.jpeg")))
